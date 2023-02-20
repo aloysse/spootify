04a3791cee2624e420dea26418168639d7f1b9ae
aloysse <shiou.ho@gmail.com>
2023-02-21 07:27:32 +0800

    取消 StricMode

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import reducer,{ initailState } from './utils/reducer'
 import  { StateProvider } from './utils/StateProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <StateProvider  initailState={initailState} reducer={reducer}>
     <App />
     </StateProvider>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
