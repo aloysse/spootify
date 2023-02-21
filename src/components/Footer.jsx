c626bf70fb63e33febc7495fb19015dbb8a94297
aloysse <shiou.ho@gmail.com>
2023-02-21 09:17:35 +0800

    新增 音量控制元件

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import CurrentTrack from './CurrentTrack'
 import PlayerControls from './PlayerControls'
+import Volume from './Volume'
 
 const Footer = () => {
   return (
     <div className='bg-gray-900 border-t border-gray-700 text-white h-full grid grid-cols-3 items-center p-3'>
       <CurrentTrack/>
       <PlayerControls/>
-      <div></div>
+      <Volume/>
     </div>
   )
 }
