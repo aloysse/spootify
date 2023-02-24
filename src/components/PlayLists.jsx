d38b2c985ae05e371d5d5eaa80a223b1d104d2d2
aloysse <shiou.ho@gmail.com>
2023-02-24 11:38:46 +0800

    新增 playlist actived 樣式

--- src/components/PlayLists.jsx
+++ src/components/PlayLists.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import axios from 'axios'
 import { useStateProvider } from '../utils/StateProvider'
 import {spotifyAPI} from '../utils/spotify'
@@ -17,7 +17,7 @@ const PlayLists = () => {
                 }
             });
             const {items} = response.data;
-            const playlists = items.map(({name,id})=>({name,id}))
+            const playlists = items.map(({name,id})=>({name,id,actived:false}))
             dispatch({
                 type:reducerCases.SET_PLAYLISTS,
                 playlists
@@ -29,14 +29,27 @@ const PlayLists = () => {
 
     const changeCurrentPlaylist = (selectedPlaylistId) => {
         dispatch({type: reducerCases.SET_PLAYINGLIST_ID, selectedPlaylistId})
+        playlists.forEach(item => {
+            if(item.id === selectedPlaylistId){
+                item.actived = true;
+            }else{
+                item.actived = false;
+            }
+        });
+        dispatch({
+            type:reducerCases.SET_PLAYLISTS,
+            playlists
+        })
     }
 
   return (
     <div className='relative h-full overflow-y-auto scrollbar-hide'>
         <ul className='absolute w-full'>
-        {playlists.map(({name,id})=>(
-            <li onClick={()=>changeCurrentPlaylist(id)} key={id} className="whitespace-nowrap text-ellipsis overflow-hidden mb-3 text-gray-400 hover:text-white cursor-pointer">{name}</li>
-        ))}
+        {playlists.map(({name,id,actived})=>{
+            // const [actived] = activedItems.filter(item=>item.id===id);
+            // console.log(activedItems);
+            return <li onClick={()=>changeCurrentPlaylist(id)} key={id} className={`whitespace-nowrap text-ellipsis overflow-hidden mb-3 ${actived ? 'text-gray-100' :'text-gray-400'} hover:text-white cursor-pointer`}>{name}</li>
+        })}
         </ul></div>
   )
 }
