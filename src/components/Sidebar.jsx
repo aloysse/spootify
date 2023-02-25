5aa7b166c6ef96e31594ad979524c4e4330e61d6
aloysse <shiou.ho@gmail.com>
2023-02-25 11:27:10 +0800

    增加 logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {RiPlayList2Line,RiHome4Line,RiSearchLine} from 'react-icons/ri'
+import logo from '../assets/spootify_logo.svg';
 import PlayLists from './PlayLists'
 
 const sidebarContents = [
@@ -9,7 +10,8 @@ const sidebarContents = [
 const Sidebar = () => {
   return (
     <div className='bg-black text-white p-5 pb-0 flex flex-col'>
-      <ul className='flex flex-col gap-3 mb-5'>
+      <img className='w-full py-3 mb-3' src={logo} alt="" />
+      <ul className='flex flex-col gap-3 mb-5 text-gray-400'>
         <li className='flex cursor-pointer gap-3 items-center'><RiHome4Line className='text-2xl'/><p className='text-base'>首頁</p></li>
         <li className='flex cursor-pointer gap-3 items-center'><RiSearchLine className='text-2xl'/><p className='text-base'>搜尋</p></li>
         <li className='flex cursor-pointer gap-3 items-center'><RiPlayList2Line className='text-2xl'/><p className='text-base'>你的音樂庫</p></li>
